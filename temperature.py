5da98ac4576b5c9d596132a63324c838be0da71b
Andrew Kim <andrewkim229@gmail.com>
2023-07-15 14:29:09 -0400

    second try

--- temperature.py
+++ temperature.py
@@ -1,31 +1,38 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/env python
 
-# Set up GPIO mode and pin number
-GPIO.setmode(GPIO.BOARD)
-pin_number = 4  # Replace with your GPIO pin number
+import glob
+import time
+import datetime
 
-# Set up GPIO pin as input
-GPIO.setup(pin_number, GPIO.IN)
+def read_temp(decimals=1, sleeptime=3):
+    """Reads the temperature from a 1-wire device"""
+    device_list = glob.glob("/sys/bus/w1/devices/" + "28*")
+    if not device_list:
+        print("No 1-wire devices found.")
+        return
 
-# Read the GPIO value
+    device = device_list[0] + "/w1_slave"
 
-while True:
-    gpio_value = GPIO.input(pin_number)
-    print(gpio_value)
+    try:
+        while True:
+            timepoint = datetime.datetime.now()
+            with open(device, "r") as f:
+                lines = f.readlines()
 
-# Generate HTML content
-# html_content = f"""
-# <html>
-#   <head>
-#     <title>GPIO Reading</title>
-#   </head>
-#   <body>
-#     <h1>GPIO Reading</h1>
-#     <p>GPIO Value: {gpio_value}</p>
-#   </body>
-# </html>
-# """
+            while lines[0].strip()[-3:] != "YES":
+                time.sleep(0.2)
+                with open(device, "r") as f:
+                    lines = f.readlines()
 
-# Write HTML content to the file
-# with open('/var/www/html/index.html', 'w') as html_file:
-#     html_file.write(html_content)
+            timepassed = (datetime.datetime.now() - timepoint).total_seconds()
+            equals_pos = lines[1].find("t=")
+            if equals_pos != -1:
+                temp_string = lines[1][equals_pos + 2:]
+                temp = round(float(temp_string) / 1000.0, decimals)
+                print(time.strftime("%d/%m/%y@%H:%M:%S - ") + str(temp) + " C")
+                time.sleep(sleeptime - timepassed)
+    except KeyboardInterrupt:
+        print("Temperature reading stopped by user.")
+
+if __name__ == "__main__":
+    read_temp()
